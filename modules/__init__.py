1f0ff75f0116014652b51a03932fcd3669e4a170
Chen Li <lichen000426@gmail.com>
2024-03-02 23:51:34 -0800

    encode, decode, binarizer network architecture

---
+++ modules/__init__.py
@@ -0,0 +1,2 @@
+from .conv_rnn import ConvLSTMCell # LSTM Conv cell
+from .sign import Sign
---
+++ network.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from modules import ConvLSTMCell, Sign
+
+'''
+The Network Architecture is based on Figure 1
+
+Decoder network's Depth-to-Space is implemented by using
+torch.nn.functional.pixel_shuffle().
+Depth-to-Space is used to reduce the depth during the decoding
+process. It is discussed in detail at Paragraph 5, Section 2.
+
+For an output with size (512, H, W),
+it will be rearranged to (512/2/2, H*2, W*2) after applied pixel_shuffle.
+'''
+class EncoderCell(nn.Module):
+    def __init__(self):
+        super(EncoderCell, self).__init__()
+
+        self.conv = nn.Conv2d(
+            3, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.rnn1 = ConvLSTMCell(
+            64,
+            256,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+        self.rnn2 = ConvLSTMCell(
+            256,
+            512,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+        self.rnn3 = ConvLSTMCell(
+            512,
+            512,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+
+    def forward(self, input, hidden1, hidden2, hidden3):
+        x = self.conv(input)
+
+        hidden1 = self.rnn1(x, hidden1)
+        x = hidden1[0]
+
+        hidden2 = self.rnn2(x, hidden2)
+        x = hidden2[0]
+
+        hidden3 = self.rnn3(x, hidden3)
+        x = hidden3[0]
+
+        return x, hidden1, hidden2, hidden3
+
+
+class Binarizer(nn.Module):
+    def __init__(self):
+        super(Binarizer, self).__init__()
+        self.conv = nn.Conv2d(512, 32, kernel_size=1, bias=False)
+        self.sign = Sign()
+
+    def forward(self, input):
+        feat = self.conv(input)
+        x = F.tanh(feat)
+        return self.sign(x)
+
+
+class DecoderCell(nn.Module):
+    def __init__(self):
+        super(DecoderCell, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            32, 512, kernel_size=1, stride=1, padding=0, bias=False)
+        self.rnn1 = ConvLSTMCell(
+            512,
+            512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+        self.rnn2 = ConvLSTMCell(
+            128,
+            512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+        self.rnn3 = ConvLSTMCell(
+            128,
+            256,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=3,
+            bias=False)
+        self.rnn4 = ConvLSTMCell(
+            64,
+            128,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=3,
+            bias=False)
+        self.conv2 = nn.Conv2d(
+            32, 3, kernel_size=1, stride=1, padding=0, bias=False)
+
+    def forward(self, input, hidden1, hidden2, hidden3, hidden4):
+        x = self.conv1(input)
+
+        hidden1 = self.rnn1(x, hidden1)
+        x = hidden1[0]
+        x = F.pixel_shuffle(x, 2)
+
+        hidden2 = self.rnn2(x, hidden2)
+        x = hidden2[0]
+        x = F.pixel_shuffle(x, 2)
+
+        hidden3 = self.rnn3(x, hidden3)
+        x = hidden3[0]
+        x = F.pixel_shuffle(x, 2)
+
+        hidden4 = self.rnn4(x, hidden4)
+        x = hidden4[0]
+        x = F.pixel_shuffle(x, 2)
+
+        x = F.tanh(self.conv2(x)) / 2
+        return x, hidden1, hidden2, hidden3, hidden4
