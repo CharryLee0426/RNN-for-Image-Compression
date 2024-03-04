7a5a645487968caa802bc3c41469a14351ca2fcf
Chen Li <lichen000426@gmail.com>
2024-03-03 20:07:47 -0800

    test scripts

--- test/get_kodak.sh
+++ test/get_kodak.sh
@@ -3,7 +3,7 @@
 
 mkdir -p test/images
 
-for i in {0..24..1}; do
+for i in {01..24..1}; do
     echo ${i}
     wget http://r0k.us/graphics/kodak/kodak/kodim${i}.png -O test/images/kodim${i}.png
 done
